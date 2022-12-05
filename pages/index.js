40f44c84d59454a4761e409c0574c8ac030e38d3
jeonchaerin <chaerin0411@naver.com>
2022-12-05 21:56:34 +0900

    feat: Add dynamic routes

--- pages/index.js
+++ pages/index.js
@@ -1,42 +1,68 @@
-import Head from "next/head";
-import { useEffect, useState } from "react";
-import styles from "../styles/Home.module.css";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import Seo from "../components/Seo";
 
 export default function Home({ results }) {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Next Movies</title>
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>Movie</h1>
-
-        <p className={styles.description}>
-          {results?.map((movie) => (
-            <div key={movie.id} className={styles.movie}>
-              <img
-                className={styles.movieImg}
-                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-              />
-              <h4>{movie.original_title}</h4>
-            </div>
-          ))}
-        </p>
-      </main>
-
-      <footer className={styles.footer}>chaerin</footer>
-    </div>
-  );
+	const router = useRouter();
+	const onClick = (id, title) => {
+		router.push(`/movies/${title}/${id}`);
+	};
+	return (
+		<div className="container">
+			<Seo title="Home" />
+			{results?.map((movie) => (
+				<div
+					onClick={() => onClick(movie.id, movie.original_title)}
+					className="movie"
+					key={movie.id}
+				>
+					<img
+						src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+					/>
+					<h4>
+						<Link legacyBehavior
+							href={`/movies/${movie.original_title}/${movie.id}`}
+						>
+							<a>{movie.original_title}</a>
+						</Link>
+					</h4>
+				</div>
+			))}
+			<style jsx>{`
+				.container {
+					display: grid;
+					grid-template-columns: 1fr 1fr;
+					padding: 20px;
+					gap: 20px;
+				}
+				.movie {
+					cursor: pointer;
+				}
+				.movie img {
+					max-width: 100%;
+					border-radius: 12px;
+					transition: transform 0.2s ease-in-out;
+					box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+				}
+				.movie:hover img {
+					transform: scale(1.05) translateY(-10px);
+				}
+				.movie h4 {
+					font-size: 18px;
+					text-align: center;
+				}
+			`}</style>
+		</div>
+	);
 }
 
 export async function getServerSideProps() {
-  const { results } = await (
-    await fetch(`http://localhost:3000/api/movies`)
-  ).json();
-  return {
-    props: {
-      results,
-    },
-  };
+	const { results } = await (
+		await fetch(`http://localhost:3000/api/movies`)
+	).json();
+	return {
+		props: {
+			results,
+		},
+	};
 }
